863e361716ad8f102ae7f3817901c9486a42e07b
Manikant Rai <manikant.rai2000@gmail.com>
2021-03-05 00:54:34 +0530

    intent added

--- app/src/main/java/com/tutorials/eu/activitydemo/MainActivity.kt
+++ app/src/main/java/com/tutorials/eu/activitydemo/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.tutorials.eu.activitydemo
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 // TODO Step 1: Create a new project with Empty Activity and explain the project structure and all the things that you have explained in the Presentation.
@@ -10,6 +12,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnsubmit = findViewById<Button>(R.id.button)
+        btnsubmit.setOnClickListener{
+            val intent = Intent(this,AnotherActivity::class.java)
+            startActivity(intent)
+        }
 
         // TODO Step 2: Override all the lifecycle methods and print the log in it. After that just run and see which log is printed at what time.
         Log.e("onCreate method", "is called...")
@@ -44,4 +51,6 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         Log.e("onDestroy method", "is called...")
     }
+
+
 }
