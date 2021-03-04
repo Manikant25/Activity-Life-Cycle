b32ec818b1fb8c4bca8aa1ccc2963ebc1efa26db
Manikant Rai <manikant.rai2000@gmail.com>
2021-03-05 00:20:53 +0530

    first commit

---
+++ app/src/main/java/com/tutorials/eu/activitydemo/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.tutorials.eu.activitydemo
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+// TODO Step 1: Create a new project with Empty Activity and explain the project structure and all the things that you have explained in the Presentation.
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // TODO Step 2: Override all the lifecycle methods and print the log in it. After that just run and see which log is printed at what time.
+        Log.e("onCreate method", "is called...")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e("onStart method", "is called...")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e("onResume method", "is called...")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e("onPause method", "is called...")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.e("onStop method", "is called...")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.e("onRestart method", "is called...")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("onDestroy method", "is called...")
+    }
+}
